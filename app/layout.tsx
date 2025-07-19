3a2999f7f2138a4dd9b473f0dc4d99a544b19534
hamzaa-bouyahyaa <hamzabouyahya198@gmail.com>
2025-07-19 15:38:31 +0100

    the first version of landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import favIcon from "@/app/assets/favicon.png";
 
 export const metadata: Metadata = {
   title: "Neov",
   description: "Investing in the future",
+  icons: {
+    icon: favIcon.src,
+  },
 };
 
 export default function RootLayout({
